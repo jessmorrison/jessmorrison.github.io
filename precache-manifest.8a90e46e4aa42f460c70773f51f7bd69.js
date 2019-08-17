6207e53ae0ce84564bc2cd31fe059a541f217458
jessmorrison <jessmorrison90@gmail.com>
2019-08-17 14:29:15 -0500

    Updates

---
+++ precache-manifest.8a90e46e4aa42f460c70773f51f7bd69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916eb4a8574969a83d778dfafb363d4a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fc2debdad3971199ed78",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1b4d327e10de82eb92f7",
+    "url": "/portfolio/static/js/2.a31939dd.chunk.js"
+  },
+  {
+    "revision": "fc2debdad3971199ed78",
+    "url": "/portfolio/static/js/main.493a1b9f.chunk.js"
+  },
+  {
+    "revision": "5029282666060c428c88",
+    "url": "/portfolio/static/js/runtime~main.cbff171f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
